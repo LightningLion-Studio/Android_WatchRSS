dbb6058f539ddb26f5d59955e5de75fdcc0f457c
gfhdhytghd <3391146750@qq.com>
2026-07-13 17:17:36 +0800

    更新 release 1.2.3-1 版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.lightningstudio.watchrss"
         minSdk = 30
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.2.2-1"
+        versionCode = 20
+        versionName = "1.2.3-1"
         buildConfigField("boolean", "ENABLE_RUNTIME_PERF_MONITOR", "false")
         buildConfigField("boolean", "ENABLE_WATCH_DEBUG_MASK", "false")
         manifestPlaceholders["debugActivityExported"] = "false"
