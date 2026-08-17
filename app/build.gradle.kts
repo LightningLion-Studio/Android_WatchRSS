5d419ad127affd843f6fa74bc1b0f381581eaab2
gfhdhytghd <3391146750@qq.com>
2026-08-17 23:14:04 +0800

    更新 release 1.3.2-5 版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "com.lightningstudio.watchrss"
         minSdk = 30
         targetSdk = 34
-        versionCode = 31
+        versionCode = 32
         versionName = "1.3.2-5"
         buildConfigField("String", "WATCHRSS_OPENPANEL_CLIENT_ID", "\"3b151c92-b189-48a3-ae77-148db3235ca1\"")
         buildConfigField("String", "WATCHRSS_OPENPANEL_CLIENT_SECRET", "\"\"")
